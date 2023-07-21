a887f43704e28a9e0e21f255ef8df54ead7f8870
Ilya <iliaabr5@gmail.com>
2023-07-21 19:30:03 +0300

    Initial commit

---
+++ src/main/java/com/example/weathertelegrambot/WeatherTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.weathertelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherTelegramBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherTelegramBotApplication.class, args);
+    }
+
+}
